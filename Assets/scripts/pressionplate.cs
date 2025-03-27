fff5d8d181bf9bf32ba1e633b08c9da8ca142d48
javchy <romingouf@gmail.com>
2025-03-27 11:50:51 +0100

    alpha0.3

--- Assets/scripts/pressionplate.cs
+++ Assets/scripts/pressionplate.cs
@@ -3,13 +3,22 @@ using UnityEngine.Events;
 
 public class pressionplate : MonoBehaviour
 {
+    private Animator _animator;
 
-   
 
     [SerializeField]
     private UnityEvent _collisionEntered;
-  
 
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    [ContextMenu("activate")]
+    public void activate()
+    {
+        _animator.SetTrigger("activate");
+    }
     private void OnCollisionEnter2D(Collision2D col)
     {
         
